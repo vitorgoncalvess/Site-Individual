5c3b5af7cf2105961306537cfe0e88bd37eacdb9
vitorgoncalvess <vitor.gsilva@sptech.school>
2022-06-01 22:18:33 -0300

    inicio mines

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sql = require('mssql');
 var mySqlConfig = {
     host: "localhost",
     database: "midasGain",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "1991vitor1",
 };
 
 function executar(instrucao) {
